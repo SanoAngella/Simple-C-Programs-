fe180f02b684d24fdc6c0916eeb936e4ca1549cf
Angella <sanoangella9@gmail.com>
2026-02-04 12:48:09 -0600

    Add recursive function to print numbers 1 to 50

---
+++ recursion-50.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void printNumbers(int n) {
+    if (n > 50)
+        return;
+    printf("%d ", n);
+    printNumbers(n + 1);
+}
+
+int main() {
+    printNumbers(1);
+    return 0;
+}
